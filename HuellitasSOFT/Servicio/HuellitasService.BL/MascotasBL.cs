cab459a1635e374a214dd0721b194bd5f020893c
luisdaendo <enriquedominguezluisdavid@gmail.com>
2019-05-28 22:02:27 -0500

    Huellitas

---
+++ HuellitasSOFT/Servicio/HuellitasService.BL/MascotasBL.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Huellitas.DTO.Mascotas;
+using HuellitasService.Common.Interfaces;
+using HuellitasService.Common.Resources;
+using HuellitasService.DAO;
+
+namespace HuellitasService.BL
+{
+    public class MascotasBL : IMascotaBL
+    {
+        public MascotaDTO GuardarMascota(MascotaDTO mascota)
+        {
+            try
+            {
+                mascota = new MascotasDAO().CrearMascota(mascota);
+                mascota.Estado = Status.Success;
+            }
+            catch (Exception e)
+            {
+                mascota.Estado = Status.Error;
+                mascota.Msg = MensajesError.ErrorGeneral;
+            }
+            return mascota;
+        }
+
+        public List<MascotaDTO> ListarMascotas()
+        {
+            List<MascotaDTO> mascotas = new List<MascotaDTO>();
+            try
+            {
+                mascotas = new MascotasDAO().ReadMascotas();
+            }
+            catch (Exception e)
+            {
+                 
+            }
+            return mascotas;
+            
+        }
+
+        public MascotaDTO ActualizarMascota(MascotaDTO mascota)
+        {
+            try
+            {
+                mascota = new MascotasDAO().UpdateMascota(mascota);
+                mascota.Estado = Status.Success;
+            }
+            catch (Exception e)
+            {
+                mascota.Estado = Status.Error;
+                mascota.Msg = MensajesError.ErrorGeneral;
+            }
+            return mascota;
+        }
+
+        public MascotaDTO EliminarMascota(MascotaDTO mascota)
+        {
+            try
+            {
+                mascota = new MascotasDAO().DeleteMascota(mascota);
+                mascota.Estado = Status.Success;
+            }
+            catch (Exception e)
+            {
+                mascota.Estado = Status.Error;
+                mascota.Msg = MensajesError.ErrorGeneral;
+            }
+            return mascota;
+        }
+    }
+}
---
+++ HuellitasSOFT/Servicio/HuellitasService/Controllers/MascotasController.cs
@@ -0,0 +1,42 @@
+﻿using Huellitas.DTO.Mascotas;
+using HuellitasService.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HuellitasService.Controllers
+{
+    public class MascotasController : ApiController
+    {
+        private IMascotaBL service;
+        public MascotasController(IMascotaBL service)
+        {
+            this.service = service;
+        }
+        // GET: api/Mascotas
+        public List<MascotaDTO> Get()
+        {
+            return service.ListarMascotas();
+        }
+
+    
+
+        // POST: api/Mascotas
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Mascotas/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Mascotas/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
